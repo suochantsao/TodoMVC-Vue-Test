4345dc76814dec738841f1fee97063aec3417a72
suochan <suochantsao@gmail.com>
2021-03-04 12:23:17 +0800

    新增/移除todo 兩個功能

--- all.js
+++ all.js
@@ -1,9 +1,37 @@
 let todoList = new Vue({
     el:'#app',
     data:{
-        str:'hello',
+        newTodo: 'HelloWorld',
+        editedTodo: null,
+        todos: []
     },
     methods:{
+        // 新增todo
+        addTodo:function(){
+            let todoItem = {
+                "completed":false
+            };
 
+            if(this.newTodo.trim() === '' ){
+                Swal.fire(`請先輸入待辦事項`);
+            }
+            else{
+                todoItem.content = this.newTodo;
+                this.todos.push(todoItem);
+            }
+            this.newTodo = '';
+
+        },
+        // 移除指定的todo
+        removeTodo:function(item){
+            let currentIdx = this.todos.indexOf(item);
+            this.todos.splice(currentIdx,1);
+        },
+        // 修改todo的內容
+        editTodo:function(item){
+            
+            console.log(item.content);
+        }
     }
+
 })
