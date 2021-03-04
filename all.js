e0170c511a012981be33e3f3945df8df15de6039
suochan <suochantsao@gmail.com>
2021-03-04 17:57:25 +0800

    選取/刪除 指定的todo

--- all.js
+++ all.js
@@ -1,36 +1,76 @@
-let todoList = new Vue({
+const vm = new Vue({
     el:'#app',
     data:{
         newTodo: 'HelloWorld',
         editedTodo: null,
-        todos: []
+        editContent: {},
+        activeTodos: [],
+        completeTodos: []
     },
     methods:{
         // 新增todo
-        addTodo:function(){
+        addTodo(){
             let todoItem = {
                 "completed":false
             };
+            let value    = this.newTodo.trim();
 
-            if(this.newTodo.trim() === '' ){
+            if(value === '' ){
                 Swal.fire(`請先輸入待辦事項`);
+                return;
             }
             else{
-                todoItem.content = this.newTodo;
-                this.todos.push(todoItem);
+                todoItem.content = value;
+                this.activeTodos.push(todoItem);
             }
             this.newTodo = '';
 
         },
         // 移除指定的todo
-        removeTodo:function(item){
-            let currentIdx = this.todos.indexOf(item);
-            this.todos.splice(currentIdx,1);
+        removeTodo(todo){
+            let currentIdx = this.activeTodos.indexOf(todo);
+            this.activeTodos.splice(currentIdx,1);
+        },
+        removeDoneTodo(todo){
+            let currentIdx = this.completeTodos.indexOf(todo);
+            this.completeTodos.splice(currentIdx,1);
         },
         // 修改todo的內容
-        editTodo:function(item){
-            
-            console.log(item.content);
+        editTodo(todo){
+            this.editContent = todo;
+            this.editedTodo  = todo.content;
+        },
+        // 完成todo的修改
+        doneTodo(todo){
+            let value    = this.editedTodo.trim();
+
+            if( value === ''){
+                Swal.fire(`內容不能為空的`);
+                return;
+            }
+            else{
+                todo.content = this.editedTodo;
+                this.editContent = {};
+            }
+        },
+        selectTodo(todo){
+            console.log("MDFK NMSL");
+            let currentIdx = this.activeTodos.indexOf(todo);
+            this.completeTodos.push(todo);
+            this.activeTodos.splice(currentIdx,1);
+        },
+        cancleTodo(todo){
+            let currentIdx = this.completeTodos.indexOf(todo);
+            this.activeTodos.push(todo);
+            this.completeTodos.splice(currentIdx,1);
+        },
+        delSelTodo(){
+            this.completeTodos = [];
+        },
+        selAllTodo(){
+            let listLength = this.activeTodos.length;
+            this.completeTodos = this.activeTodos.slice(0,listLength);
+            this.activeTodos = [];
         }
     }
 
